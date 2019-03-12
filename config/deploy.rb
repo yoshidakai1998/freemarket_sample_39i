f1ea484cc722ab7f123e3c7b86e17c69b87554ae
KatsuhiroSaito <katuhiro_sub-106@yahoo.co.jp>
2019-03-12 22:02:07 +0900

    config/deploy.rbをカリキュラムに沿って編集
    10行目のset :rbenv_rubyの部分は2.3.1から2.5.1に変更した。

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,27 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "freemarket_sample_39i"
+set :repo_url, "git@github.com:KatsuhiroSaito/freemarket_sample_39i.git"
 
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/key_pair_freemarket.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
